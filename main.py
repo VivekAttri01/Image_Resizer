1beeab62d39a372f204d23d925543aee0877463b
Vivek Sharma <138556190+VivekAttri01@users.noreply.github.com>
2023-12-26 02:25:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+source="landscape.webp"
+destination="newImage.webp"
+img = cv2.imread(source, cv2.IMREAD_UNCHANGED)
+
+# print('Original Dimensions : ', img.shape)
+
+scale_percent = 60  # percent of original size
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+# resize image
+resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+# print('Resized Dimensions : ', resized.shape)
+
+cv2.imwrite(destination, resized)
+cv2.imshow(destination, resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
